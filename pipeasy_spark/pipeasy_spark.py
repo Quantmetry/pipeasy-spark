10899defb71fc4d2929c74c89681ee0030a4a08b
oessaid <oessaid@quantmetry.com>
2018-10-18 23:00:04 +0200

    Docstrings added to the main module.

--- pipeasy_spark/pipeasy_spark.py
+++ pipeasy_spark/pipeasy_spark.py
@@ -4,7 +4,42 @@
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler, StandardScaler
 
+'''
+The pipeasy-spark package provides a set of convenience classes and functions that
+make it easier to map each column of a Spark dataframe (or subsets of columns) to
+user-specified transformations. Increasingly complex features are provided:
+
+map_by_dtypes()     allows a simple mapping of features to user-specified transformations
+                    according to their dtypes. Each dtype is assigned the sequence of
+                    transformations passed in the arguments as dictionaries.
+
+map_by_column()     allows mapping transformations at a more detailed level. Each column
+                    of the dataframe (or subset thereof) can be assigned a specific
+                    sequence of transformations.
+
+Each function returns a Spark dataframe that is ready to be used by Spark.ml
+'''
+
 def map_by_dtypes(df_pipe, target_name):
+    '''
+    Maps the columns of a dataframe to specific transformations depending on their
+    dtype.
+
+    Categorical columns are taken through a StringIndexer > OneHotEncoder
+    sequence and numerical ones through a VectorAssembler > StandardScaler sequence.
+    The target variable is set in the arguments and a StringIndexer is applied to it.
+    The transformed features are then assembled by a VectorAssembler and a transformed
+    dataframe is returned, ready to be used in spark.ml
+
+    Parameters:
+
+    df_pipe         a Spark dataframe to be transformed
+    target_name     the name of the target column
+
+    TODO:
+    Make the user choose which transformations are to be applied for each dtype.
+    Maybe pass dtypes as dictionaries as opposed to the string/not string dichotomy.
+    '''
     cat_columns = [item[0] for item in df_pipe.dtypes if item[1]=='string']
     num_columns = [item[0] for item in df_pipe.dtypes if (not item[1]=='string'
                                                           and not item[0]==target_name)]
