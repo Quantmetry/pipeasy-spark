dc4e38be0b0e87c42fa3da482a0675981de63296
oessaid <oessaid@quantmetry.com>
2018-10-27 13:13:28 +0200

    First draft of pyspark.ml transformers as arguments, amended docstring.

--- pipeasy_spark/pipeasy_spark.py
+++ pipeasy_spark/pipeasy_spark.py
@@ -24,8 +24,9 @@ def map_by_dtypes(df_pipe, target_name, cat_transformers, num_transformers):
     '''Maps the columns of a dataframe to specific transformations depending on their
     dtype.
 
-    Categorical columns are taken through a StringIndexer > OneHotEncoder
-    sequence and numerical ones through a VectorAssembler > StandardScaler sequence.
+    Categorical columns are taken through the cat_stransformers sequence
+    (StringIndexer > OneHotEncoder for example) and numerical ones through the
+    num_transformers sequence (VectorAssembler > StandardScaler for example).
     The target variable is set in the arguments and a StringIndexer is applied to it.
     The transformed features are then assembled by a VectorAssembler and a pyspark.ml
     pipeline object is returned.
@@ -38,9 +39,19 @@ def map_by_dtypes(df_pipe, target_name, cat_transformers, num_transformers):
     num_transformers    list of pyspark.ml transformers for numerical columns
 
     TODO:
+    Handle unseen labels.
+
+    DONE:
     Make the user choose which transformations are to be applied for each dtype.
     Maybe pass dtypes as dictionaries as opposed to the string/not string dichotomy.
-    Handle unseen labels.
+
+    Tested with the following transformers :
+    import pipeasy_spark as ppz
+    from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler, StandardScaler
+    ppz_pipeline= ppz.map_by_dtypes(df_pipe,
+                                    target_name='Survived',
+                                    cat_transformers=[StringIndexer, OneHotEncoderEstimator],
+                                    num_transformers=[VectorAssembler, StandardScaler])
 
     '''
     cat_columns = [item[0] for item in df_pipe.dtypes if item[1]=='string']
@@ -60,12 +71,13 @@ def map_by_dtypes(df_pipe, target_name, cat_transformers, num_transformers):
         # Chain transformers
         cat_column_stages = []
         for idx, transformer in enumerate(cat_transformers):
-            if idx==0:
-                transformer_args = dict(inputCol=cat_column, outputCol=cat_column+'_indexed')
+            if idx == 0:
+                transformer_args = dict(
+                    inputCol=cat_column, outputCol=cat_column + '_indexed')
             else:
-                transformer_args = dict(inputCols=[cat_transformers[idx-1].getOutputCol()], outputCols=[cat_column+'_transformed'])
-            step = transformer(**transformer_args)
-            cat_column_stages += step
+                transformer_args = dict(
+                    inputCols=[cat_column_stages[idx - 1].getOutputCol()], outputCols=[cat_column + '_transformed'])
+            cat_column_stages += [transformer(**transformer_args)]
 
         # Add stages to main stages list
         stages += cat_column_stages
@@ -84,12 +96,13 @@ def map_by_dtypes(df_pipe, target_name, cat_transformers, num_transformers):
         # Chain transformers
         num_column_stages = []
         for idx, transformer in enumerate(num_transformers):
-            if idx==0:
-                transformer_args = dict(inputCol=num_column, outputCol=num_column+'_assembled')
+            if idx == 0:
+                transformer_args = dict(
+                    inputCols=[num_column], outputCol=num_column + '_assembled')
             else:
-                transformer_args = dict(inputCols=num_transformers[idx-1].getOutputCol(), outputCols=num_column+'_scaled')
-            step = transformer(**transformer_args)
-            num_column_stages += step
+                transformer_args = dict(
+                    inputCol=num_column_stages[idx - 1].getOutputCol(), outputCol=num_column + '_scaled')
+            num_column_stages += [transformer(**transformer_args)]
 
         # Add stages to main stages list
         stages += num_column_stages
