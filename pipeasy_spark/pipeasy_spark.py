322b88738b5dc43ee1abbfa08cd3bfe6d6e98b63
Benjamin Habert <bhabert@quantmetry.com>
2019-01-09 13:24:27 +0100

    implementation of map_by_column

--- pipeasy_spark/pipeasy_spark.py
+++ pipeasy_spark/pipeasy_spark.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import pyspark
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import (
     StringIndexer,
@@ -22,6 +23,119 @@ map_by_column()     allows mapping transformations at a more detailed level. Eac
 Each function returns a pyspark.ml Pipeline object.
 
 """
+
+
+def map_by_column(columns_mapping, target_name=None):
+    """Create a dataframe transformation pipeline.
+
+    Example
+    -------
+
+        >>> df.show(1)
+        ....
+        >>> pipeline = map_by_column({
+                'species': [StringIndexer(), OneHotEncoder()],
+                'sepal_length': [StandardScaler()],
+        })
+        >>> df_transormed = pipeline.fit_transform(df)
+
+
+    Parameters
+    ----------
+        columns_mapping: dict
+        target_name: str
+            name of the target column that will be converted using StringIndexer()
+
+    Returns
+    -------
+        pipeline: a pyspark pipeline
+    """
+    stages = []
+    columns_to_drop = []
+    columns_to_rename = []
+
+    for column, transformers in columns_mapping.items():
+        if transformers:
+            temp_column_names = [column]
+            for transformer in transformers:
+                previous_column = temp_column_names[-1]
+                next_column = previous_column + '_' + transformer.__class__.__name__
+                temp_column_names.append(next_column)
+
+                transformer = set_transformer_in_out(transformer, previous_column, next_column)
+                stages.append(transformer)
+            
+            # all the temporary columns should be dropped except the last one
+            columns_to_drop += temp_column_names[:-1]
+            # the last temporary column should be renamed to the original name
+            columns_to_rename.append(
+                (temp_column_names[-1], temp_column_names[0])
+            )
+    for column in columns_to_drop:
+        stages.append(ColumnDropper(inputCol=column))
+    for original, new in columns_to_rename:
+        stages.append(ColumnRenamer(original, new))
+
+    if target_name:
+        stages.append(FeatureBuilder(targetCol=target_name))
+    # Create a Pipeline
+    pipeline = Pipeline(stages=stages)
+    return pipeline
+
+
+def set_transformer_in_out(transformer, inputCol, outputCol):
+    transformer = transformer.copy()
+    try:
+        transformer.setInputCol(inputCol)
+    except AttributeError:
+        try:
+            transformer.setInputCols([inputCol])
+        except AttributeError:
+            raise ValueError("Invalid transformer: " + str(transformer))
+
+    try:
+        transformer.setOutputCol(outputCol)
+    except AttributeError:
+        try:
+            transformer.setOutputCols([outputCol])
+        except AttributeError:
+            # we accept transformers that do not have an outputCol
+            # (as ColumnDropper)
+            pass
+
+    return transformer
+
+
+class ColumnDropper(pyspark.ml.Transformer, pyspark.ml.feature.HasInputCol):
+    def __init__(self, inputCol=None):
+        super().__init__()
+        self.setInputCol(inputCol)
+
+    def transform(self, dataset):
+        return dataset.drop(self.getInputCol())
+
+
+class ColumnRenamer(pyspark.ml.Transformer,
+                    pyspark.ml.feature.HasInputCol,
+                    pyspark.ml.feature.HasOutputCol):
+    def __init__(self, inputCol=None, outputCol=None):
+        super().__init__()
+        self.setInputCol(inputCol)
+        self.setOutputCol(outputCol)
+
+    def transform(self, dataset):
+        return dataset.withColumnRenamed(self.getInputCol(), self.getOutputCol())
+
+
+class FeatureBuilder(pyspark.ml.Transformer):
+    def __init__(self, targetCol):
+        super().__init__()
+        self.targetCol = targetCol
+
+    def transform(self, dataset):
+        columns_to_regroup = [col for col in dataset.columns if col != self.targetCol]
+        actual_transformer = VectorAssembler(inputCols=columns_to_regroup, outputCol='features')
+        return actual_transformer.transform(dataset)
 
 
 def map_by_dtypes(df_pipe, target_name, cat_transformers, num_transformers):
